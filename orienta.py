1cc2fea3b80a1f69ca4ed0f914c7550737d31891
Javier <javiermoralesr95@gmail.com>
2020-10-23 12:29:52 -0300

    Added bilinear local orientations

---
+++ orienta.py
@@ -0,0 +1,29 @@
+
+import argparse
+import matplotlib.pyplot as plt
+import numpy as np
+import orientation_histograms as oh
+import pai_io
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Plots local orientations.')
+    parser.add_argument('-image', type=str, help='Image to calculate local orientations.', required=True)
+    parser.add_argument('-k', type=int, help='Grid dimensions.', required=True)
+    args = parser.parse_args()
+    image = pai_io.imread(args.image, as_gray = True)
+    K = args.k
+    A1, R1 = oh.compute_local_orientations(image, K)
+    A2, R2 = oh.compute_local_orientations_bilinear(image, K)
+    ys = np.arange(K)
+    xs = np.arange(K)
+    ys = np.floor(( (ys + 0.5) / K ) * image.shape[0])
+    xs = np.floor(( (xs + 0.5) / K ) * image.shape[1]) 
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 5))
+    axes[0].imshow(image, cmap = 'gray', vmax = 255, vmin = 0)
+    axes[0].quiver(xs, ys, np.cos(A1)*R1, np.sin(A1)*R1, angles = 'xy', color = 'r')
+    axes[0].axis('off')
+    axes[1].imshow(image, cmap = 'gray', vmax = 255, vmin = 0)
+    axes[1].quiver(xs, ys, np.cos(A2)*R2, np.sin(A2)*R2, angles = 'xy', color = 'r')
+    axes[1].axis('off')
+    fig.tight_layout()
+    plt.show()
--- orientation_histograms.py
+++ orientation_histograms.py
@@ -47,7 +47,6 @@ def compute_local_orientations(image, cell_size):
     idx_rows, idx_cols = np.indices(image.shape)
     idx_grid_rows = np.floor(cell_size * idx_rows / image.shape[0])
     idx_grid_cols = np.floor(cell_size * idx_cols / image.shape[1])
-    print(idx_grid_rows)
     for p in np.arange(cell_size) :
         for q in np.arange(cell_size) :
             rows, cols = np.where((idx_grid_rows == p) & (idx_grid_cols == q))
@@ -57,6 +56,57 @@ def compute_local_orientations(image, cell_size):
             Gx_local[p,q] = np.sum((np.square(local_gx) - np.square(local_gy))) 
             Gy_local[p,q] = np.sum(2.0*(local_gx*local_gy))
             r_local[p,q] = np.mean(local_mag)
+    local_ang = arctan2(Gy_local, Gx_local) * 0.5
+    local_ang = local_ang + np.pi*0.5 # 0 <= ang  <= pi    
+    return local_ang,  r_local
+
+
+def compute_local_orientations_bilinear(image, cell_size):
+    Gx_local = np.zeros((cell_size, cell_size), dtype = np.float32)
+    Gy_local = np.zeros((cell_size, cell_size), dtype = np.float32)
+    r_local = np.zeros((cell_size, cell_size), dtype = np.float32)    
+    gx_mask = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], np.float32)
+    gy_mask = np.transpose(gx_mask)
+    gx = nd_filters.convolve(image.astype(np.float32), gx_mask)
+    gy = nd_filters.convolve(image.astype(np.float32), gy_mask)
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            p_prime = (i / image.shape[0]) * cell_size
+            q_prime = (j / image.shape[1]) * cell_size
+            l_pos = int(np.floor(p_prime - 0.5))
+            r_pos = int(np.floor(p_prime + 0.5))
+            n_pos = int(np.floor(q_prime - 0.5))
+            s_pos = int(np.floor(q_prime + 0.5))
+            if r_pos == cell_size:
+                r_pos -= 1
+            if s_pos == cell_size:
+                s_pos -= 1
+            dist_p = p_prime - np.floor(p_prime)
+            if dist_p < 0.5:
+                l_weight = 0.5 - dist_p
+                r_weight = 1 - l_weight
+            else:
+                r_weight = dist_p - 0.5
+                l_weight = 1 - r_weight
+            dist_q = q_prime - np.floor(q_prime)
+            if dist_q < 0.5:
+                n_weight = 0.5 - dist_q
+                s_weight = 1 - n_weight
+            else:
+                s_weight = dist_q - 0.5
+                n_weight = 1 - s_weight
+            Gx_local[l_pos, n_pos] += l_weight * n_weight * (np.square(gx[i, j]) - np.square(gy[i, j]))
+            Gx_local[r_pos, n_pos] += r_weight * n_weight * (np.square(gx[i, j]) - np.square(gy[i, j]))
+            Gx_local[l_pos, s_pos] += l_weight * s_weight * (np.square(gx[i, j]) - np.square(gy[i, j]))
+            Gx_local[r_pos, s_pos] += r_weight * s_weight * (np.square(gx[i, j]) - np.square(gy[i, j]))
+            Gy_local[l_pos, n_pos] += l_weight * n_weight * (2.0 * gx[i, j] * gy[i, j])
+            Gy_local[r_pos, n_pos] += r_weight * n_weight * (2.0 * gx[i, j] * gy[i, j])
+            Gy_local[l_pos, s_pos] += l_weight * s_weight * (2.0 * gx[i, j] * gy[i, j])
+            Gy_local[r_pos, s_pos] += r_weight * s_weight * (2.0 * gx[i, j] * gy[i, j])
+            r_local[l_pos, n_pos] += l_weight * n_weight * np.sqrt(np.square(gx[i, j]) + np.square(gy[i, j]))
+            r_local[r_pos, n_pos] += r_weight * n_weight * np.sqrt(np.square(gx[i, j]) + np.square(gy[i, j]))
+            r_local[l_pos, s_pos] += l_weight * s_weight * np.sqrt(np.square(gx[i, j]) + np.square(gy[i, j]))
+            r_local[r_pos, s_pos] += r_weight * s_weight * np.sqrt(np.square(gx[i, j]) + np.square(gy[i, j]))
     local_ang = arctan2(Gy_local, Gx_local) * 0.5
     local_ang = local_ang + np.pi*0.5 # 0 <= ang  <= pi    
     return local_ang,  r_local
