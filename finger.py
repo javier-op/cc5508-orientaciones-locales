7fb52af07bd11212c9440896fc7ad2a92bb9490f
Javier <javiermoralesr95@gmail.com>
2020-10-24 18:43:13 -0300

    finger.py now calculating best matches with and without bilinear interpolation

--- finger.py
+++ finger.py
@@ -7,9 +7,10 @@ import pai_io
 from pathlib import Path
 import pickle
 
-def calculate_all_fingerprints_histograms(K, L):
+
+def calculate_all_fingerprints_histograms(local_orientation_function, K, L):
     try:
-        with open('finger_orientations.pkl', 'rb') as input:
+        with open(local_orientation_function.__name__+'.pkl', 'rb') as input:
             finger_orientations = pickle.load(input)
     except (IOError, pickle.PickleError):
         finger_orientations = {}
@@ -20,27 +21,19 @@ def calculate_all_fingerprints_histograms(K, L):
         pathlist = Path('./fingerprints').rglob('*.png')
         for path in pathlist:
             image = pai_io.imread(str(path), as_gray = True)
-            ang_local, r_local = oh.compute_local_orientations_bilinear(image, K)
+            ang_local, r_local = local_orientation_function(image, K)
             finger_orientations_k[str(path)] = (ang_local, r_local)
         finger_orientations[K] = finger_orientations_k
-        with open('finger_orientations.pkl', 'wb') as output:
+        with open(local_orientation_function.__name__+'.pkl', 'wb') as output:
             pickle.dump(finger_orientations, output, pickle.HIGHEST_PROTOCOL)
     finger_histograms = {}
     for filename in finger_orientations_k:
         ang_local, r_local = finger_orientations_k[filename]
-        finger_histograms[filename] = oh.shelo(ang_local, r_local, L)
+        finger_histograms[filename] = oh.compute_orientation_histogram_lineal(ang_local, r_local, L)
     return finger_histograms
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Finds most similar fingerprints.')
-    parser.add_argument('-image', type=str, help='Input image..', required=True)
-    parser.add_argument('-k', type=int, help='Grid dimensions for local orientations.', required=True)
-    parser.add_argument('-l', type=int, help='Array dimensions for histogram.', required=True)
-    args = parser.parse_args()
-    finger_histograms = calculate_all_fingerprints_histograms(args.k, args.l)
-    image = pai_io.imread(args.image, as_gray = True)
-    ang_local, r_local = oh.compute_local_orientations_bilinear(image, args.k)
-    input_histogram = oh.shelo(ang_local, r_local, args.l)
+
+def get_top_5_histogram_matches(input_histogram, finger_histograms):
     heap = []
     for filename in finger_histograms:
         finger_histogram = finger_histograms[filename]
@@ -49,5 +42,29 @@ if __name__ == '__main__':
     results = []
     for i in range(5):
         distance, filename = heapq.heappop(heap)
+        if distance == 0:
+            distance, filename = heapq.heappop(heap)
         results.append((filename, distance))
-    print('Done.')
+    return results
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Finds most similar fingerprints.')
+    parser.add_argument('-image', type=str, help='Input image..', required=True)
+    parser.add_argument('-k', type=int, help='Grid dimensions for local orientations.', required=True)
+    parser.add_argument('-l', type=int, help='Array dimensions for histogram.', required=True)
+    args = parser.parse_args()
+    image = pai_io.imread(args.image, as_gray = True)
+
+    finger_histograms = calculate_all_fingerprints_histograms(oh.compute_local_orientations, args.k, args.l)
+    ang_local, r_local = oh.compute_local_orientations(image, args.k)
+    input_histogram = oh.compute_orientation_histogram_lineal(ang_local, r_local, args.l)
+    results_regular = get_top_5_histogram_matches(input_histogram, finger_histograms)
+
+    finger_histograms_b = calculate_all_fingerprints_histograms(oh.compute_local_orientations_bilinear, args.k, args.l)
+    ang_local_b, r_local_b = oh.compute_local_orientations_bilinear(image, args.k)
+    input_histogram_b = oh.compute_orientation_histogram_lineal(ang_local_b, r_local_b, args.l)
+    results_bilineal = get_top_5_histogram_matches(input_histogram_b, finger_histograms_b)
+
+    print(results_regular)
+    print(results_bilineal)
--- orientation_histograms.py
+++ orientation_histograms.py
@@ -2,38 +2,6 @@ import scipy.ndimage.filters as nd_filters
 import numpy as np
 from numpy import arctan2
 
-def compute_orientation_histogram_basic(image, K):
-    h = np.zeros(K, np.float32)
-    gx_mask = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], np.float32)
-    gy_mask = np.transpose(gx_mask)
-    gx = nd_filters.convolve(image.astype(np.float32), gx_mask)
-    gy = nd_filters.convolve(image.astype(np.float32), gy_mask)
-    ang = np.arctan2(gy,gx)    
-    ang[ang < 0] = ang[ang < 0] + np.pi #sin signo    
-    indx = np.round(K * ang / np.pi) 
-    indx[indx ==  K] = 0
-    for i in range(K):            
-        h[i] = np.sum(indx == i)    
-    return h    
-
-
-def compute_orientation_histogram(image, K):
-    h = np.zeros(K, np.float32)
-    gx_mask = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], np.float32)
-    gy_mask = np.transpose(gx_mask)
-    gx = nd_filters.convolve(image.astype(np.float32), gx_mask)
-    gy = nd_filters.convolve(image.astype(np.float32), gy_mask)
-    ang = np.arctan2(gy,gx)    
-    ang[ang < 0] = ang[ang < 0] + np.pi #sin signo
-    mag = np.sqrt(np.square(gy) + np.square(gx))    
-    indx = np.round(K * ang / np.pi) 
-    indx[indx ==  K] = 0
-    for i in range(K):
-        rows, cols = np.where(indx  == i)        
-        h[i] = np.sum(mag[rows, cols])
-    h =  h / np.linalg.norm(h,2)  #vector unitario    
-    return h
-
 
 def compute_local_orientations(image, cell_size):
     Gx_local = np.zeros((cell_size, cell_size), dtype = np.float32)
@@ -105,7 +73,7 @@ def compute_local_orientations_bilinear(image, cell_size):
     return local_ang,  r_local
 
 
-def shelo(ang_local, r_local, L):
+def compute_orientation_histogram_lineal(ang_local, r_local, L):
     h = np.zeros(L, np.float32)
     ang_local[ang_local < 0] = ang_local[ang_local < 0] + np.pi
     ang_prime = (L * ang_local) / np.pi
