92a1a4cf84f24701f09ae1667465b61dfd394429
Javier <javiermoralesr95@gmail.com>
2020-10-25 14:23:58 -0300

    Added evaluation mode

--- finger.py
+++ finger.py
@@ -50,23 +50,52 @@ def get_top_5_histogram_matches(input_histogram, finger_histograms):
     return results
 
 
+def get_fingerprint_to_class_map():
+    fingerprint_to_class_map = {}
+    root_path = Path('./fingerprints').glob('*')
+    for class_path in root_path:
+        for fingerprint in class_path.glob('*'):
+            fingerprint_to_class_map[str(fingerprint)] = str(class_path) 
+    return fingerprint_to_class_map
+
+
+def calculate_recover_error(local_orientation_function):
+    finger_histograms = calculate_all_fingerprints_histograms(local_orientation_function, args.k, args.l)
+    fingerprint_to_class_map = get_fingerprint_to_class_map()
+    recover_errors = []
+    for current_filename in finger_histograms:
+        current_results = get_top_5_histogram_matches(finger_histograms[current_filename], finger_histograms)
+        recover_error = 0
+        for result_filename, distance in current_results:
+            if fingerprint_to_class_map[current_filename] != fingerprint_to_class_map[result_filename]:
+                recover_error += 1.
+        recover_errors.append(recover_error)
+    return np.mean(recover_errors)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Finds most similar fingerprints.')
-    parser.add_argument('-image', type=str, help='Input image..', required=True)
+    parser.add_argument('--image', type=str, help='Input image..', required=True)
     parser.add_argument('-k', type=int, help='Grid dimensions for local orientations.', required=True)
     parser.add_argument('-l', type=int, help='Array dimensions for histogram.', required=True)
+    mode = parser.add_mutually_exclusive_group()
+    mode.add_argument('--non-bilinear', help='Calculate local orientations without bilinear interpolation.', action='store_true')
+    mode.add_argument('--eval', help='Performance evaluation mode.', action='store_true')
     args = parser.parse_args()
     image = pai_io.imread(args.image, as_gray = True)
 
-    finger_histograms = calculate_all_fingerprints_histograms(oh.compute_local_orientations, args.k, args.l)
-    ang_local, r_local = oh.compute_local_orientations(image, args.k)
-    input_histogram = oh.compute_orientation_histogram_lineal(ang_local, r_local, args.l)
-    results_regular = get_top_5_histogram_matches(input_histogram, finger_histograms)
-
-    finger_histograms_b = calculate_all_fingerprints_histograms(oh.compute_local_orientations_bilinear, args.k, args.l)
-    ang_local_b, r_local_b = oh.compute_local_orientations_bilinear(image, args.k)
-    input_histogram_b = oh.compute_orientation_histogram_lineal(ang_local_b, r_local_b, args.l)
-    results_bilineal = get_top_5_histogram_matches(input_histogram_b, finger_histograms_b)
-
-    print(results_regular)
-    print(results_bilineal)
+    if args.eval:
+        recover_error_regular = calculate_recover_error(oh.compute_local_orientations)
+        recover_error_bilinear = calculate_recover_error(oh.compute_local_orientations_bilinear)
+        print('Recover error without bilinear interpolation: ' + str(recover_error_regular))
+        print('Recover error with bilinear interpolation: ' + str(recover_error_bilinear))
+    else:
+        if args.non_bilinear:
+            local_orientation_function = oh.compute_local_orientations
+        else:
+            local_orientation_function = oh.compute_local_orientations_bilinear
+        finger_histograms = calculate_all_fingerprints_histograms(local_orientation_function, args.k, args.l)
+        ang_local, r_local = local_orientation_function(image, args.k)
+        input_histogram = oh.compute_orientation_histogram_lineal(ang_local, r_local, args.l)
+        results = get_top_5_histogram_matches(input_histogram, finger_histograms)
+        print(results)
